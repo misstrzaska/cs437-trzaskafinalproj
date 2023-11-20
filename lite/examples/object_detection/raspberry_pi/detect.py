6b929c9986015b8fa1de3f1d3dd8562c1b3bcebb
misstrzaska <weronikatrzaska@gmail.com>
2023-11-19 23:00:58 -0600

    fix if for cell phone count, print when cell found

--- lite/examples/object_detection/raspberry_pi/detect.py
+++ lite/examples/object_detection/raspberry_pi/detect.py
@@ -105,14 +105,14 @@ def run(model: str, camera_id: int, width: int, height: int, num_threads: int,
       cv2.putText(image, fps_text, text_location, cv2.FONT_HERSHEY_PLAIN,
                   font_size, text_color, font_thickness)
       percentage = probability * 100
-      #add in to count instances of cell phone detected
-      if category_name == "cell phone" and percentage > 50
+      #check if category name is cell phone and percentage is greater than 50
+      if category_name == "cell phone" and percentage > 50:
         cellcounter += 1
+        print(f"Detected a cell phone! Total cell phone count: {cellcounter}")
       #delay to achieve the desired fps
       time.sleep(0.8)
     # Stop the program if the ESC key is pressed.
     if cv2.waitKey(1) == 27:
-      print(f"Number of cell phones detected: {cellcounter}")
       break
     #actually show the image of object detection
     #cv2.imshow('object_detector', image)
